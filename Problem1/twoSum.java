32f699fff2f1997068d1cb25d9714d6e2f2114b0
Maafia Desktop <torn1998@gmail.com>
2021-09-30 16:43:49 -0400

    Completed JavaTwoSum
    JavaTwoSum solution is now completed! Using the higher quality answer from my previous C++ answer and converted it into java.

--- Problem1/twoSum.java
+++ Problem1/twoSum.java
@@ -1,5 +1,26 @@
 package Problem1;
 
+import java.util.*;
+
 public class twoSum {
-    
+    public int[] twosum(int[] nums, int target) {   
+        
+        int pair[] = new int[2];            //creating array holding 2 elements calling it pair
+
+        int size = nums.length;             //size of nums array
+
+        int difference;                     //variable holding the difference
+
+        HashMap<Integer, Integer> mapper = new HashMap<>(); //creating map
+        for(int i = 0; i < size; i++){                      //looping through the array size
+            difference = target - nums[i];                  //finding the difference between numbers and target
+            if(mapper.containsKey(difference)){             //if map has the difference we are looking for
+                pair[0] = i;                                //Updating first index with whatever i holds
+                pair[1] = mapper.get(difference);           //Updating second index with the difference
+                return pair;                                //returns pai
+            }
+            mapper.put(nums[i], i);                         //Inserts the value and index into the map
+        }
+        return pair;                                        //return pair array
+    }
 }
